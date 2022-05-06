0579ca409da23c98b064d892e8bac652d08467e0
Lucas Mortoni <lucasmortoni@outlook.com>
2022-05-06 00:36:28 -0300

    chore: adds eslint and prettier to core

--- packages/core/src/infra/http/server.ts
+++ packages/core/src/infra/http/server.ts
@@ -1,5 +1,9 @@
 import { app } from './app'
 
+app.get('/', (req, res) => {
+  return res.json({ msg: 'hello world' })
+})
+
 app.listen(3333, () => {
   console.log('tindev server is up and running 🚀')
 })
