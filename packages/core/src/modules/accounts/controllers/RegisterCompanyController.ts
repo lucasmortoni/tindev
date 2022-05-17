f7b94703eb882c13378f4667dc140ef84d4e0834
Lucas Mortoni <lucasmortoni@outlook.com>
2022-05-16 22:42:22 -0300

    feat: adds schema validation in the creation of company profile

--- packages/core/src/modules/accounts/controllers/RegisterCompanyController.ts
+++ packages/core/src/modules/accounts/controllers/RegisterCompanyController.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express'
 import { RegisterCompany } from '@accounts/usecases/RegisterCompany'
+import * as yup from 'yup'
 import { CompanyProps } from '@accounts/domain/company'
 
 export class RegisterCompanyController {
   constructor(private registerCompany: RegisterCompany) {}
   async handle(req: Request, res: Response) {
+    const dataSchema = yup.object().shape({
+      avatar_url: yup.string(),
+      name: yup.string().required(),
+      username: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup
+        .string()
+        .min(8)
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/)
+        .required()
+    })
     const data = req.body as CompanyProps
+
+    await dataSchema.validate(data, { abortEarly: false })
 
     await this.registerCompany.execute(data)
 
